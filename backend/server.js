d120a5ee11134d4db7ac76d4992a53c0aeba9b7a
Davnish singh <davnishsingh46@gmail.com>
2025-02-16 02:17:20 +0530

    redesigned bidding form

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 const cors = require('cors');
 app.use(cors({
   origin: allowedOrigins, 
-  methods: "GET,POST,PUT,DELETE",
+  methods: "GET,POST,PUT,DELETE,PATCH",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true
 }));
